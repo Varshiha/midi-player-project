a22114724981fa1b30e4328871b2a9a67a624007
Varshiha <varshihaa@icloud.com>
2025-09-12 22:59:43 -0400

    Question 47

---
+++ TuneCreator.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class TuneCreator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TuneCreator
+{
+    // instance variables - replace the example below with your own
+    private MidiPlayer player;
+
+
+    /**
+     * Constructor for objects of class TuneCreator
+     */
+    public TuneCreator()
+    {
+        // initialise instance variables
+        player = new MidiPlayer();
+    }
+
+    
+}
